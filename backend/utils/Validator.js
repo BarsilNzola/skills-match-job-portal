5e0eee834d00150d19d866e378954671cac6f819
BarsilNzola <barsiljohn@gmail.com>
2025-06-17 14:27:05 +0300

    validator

--- backend/utils/Validator.js
+++ backend/utils/Validator.js
@@ -1,14 +1,14 @@
 // utils/validators.js
 const validateJobData = (data) => {
-    if (!data.title || data.title.length < 3) {
-        return 'Job title must be at least 3 characters long';
-    }
-    if (!data.company || data.company.length < 3) {
-        return 'Company name must be at least 3 characters long';
-    }
-    if (!data.skillsRequired || !Array.isArray(data.skillsRequired)) {
-        return 'Skills required must be an array';
-    }
+   // if (!data.title || data.title.length < 3) {
+   //     return 'Job title must be at least 3 characters long';
+    //}
+    // if (!data.company || data.company.length < 3) {
+       // return 'Company name must be at least 3 characters long';
+    //}
+    //if (!data.skillsRequired || !Array.isArray(data.skillsRequired)) {
+      //  return 'Skills required must be an array';
+    //}
     return null; // No validation errors
 };
 
